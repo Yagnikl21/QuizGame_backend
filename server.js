6a05db3cf1832d769046aa7c0d242370eb5af075
Yagnik Ladani <y.m.ladnai972@gmail.com>
2025-02-02 15:23:30 +0530

    Add script

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-
-// server.js (Run this separately in Node.js)
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-
-const app = express();
-app.use(cors());
-
-app.get("/proxy", async (req, res) => {
-  try {
-    const response = await axios.get("https://api.jsonserve.com/Uw5CrX");
-      res.json(response.data);
-  } catch (error) {
-    res.status(500).send("Error fetching data");
-  }
-});
-
-app.listen(3001, () => console.log("Proxy running on port 3001"));
-// server.js (Run this separately in Node.js)
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
